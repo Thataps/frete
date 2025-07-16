bd79e0c2c9e0385286859341c2741c939a90a1fe
Thataps <thamarapaivas@gmail.com>
2025-07-16 22:54:40 +0000

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,18 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Digite o peso da sua encomenda em kg: ");
+    double peso = sc.nextDouble();
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+    if (peso < 1) {
+      System.out.println("O valor da encomenda é R$ 5,00");
+    } else if (1 < peso && peso > 5) {
+      System.out.println("O valor da encomenda é R$ 10,00");
+    } else {
+      System.out.println("O valor da encomenda é R$ 20,00");
+    }
+    
+  }
 }
